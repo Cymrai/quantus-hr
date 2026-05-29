d493caf99e1215e8b012603cca38033785e7b430
Cymrai <info@cymrai.co.uk>
2026-05-29 19:05:40 +0100

    QHR-13: Initialise NestJS backend repo with TypeScript and ESLint

---
+++ Cymrai/quantus-hr/apps/api/src/modules/user/user.entity.ts
@@ -0,0 +1,17 @@
+/**
+ * @file user.entity.ts
+ * @description Entity for the User model, used with TypeORM.
+ */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+}
