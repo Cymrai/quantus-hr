bf9c8bfd6947f6a4c43d0d7785749f7afc4179e8
Cymrai <info@cymrai.co.uk>
2026-05-29 19:49:46 +0100

    QHR-23: Implement Time Model scoring formula in the performance engi

---
+++ src/modules/performance-engine/index.ts
@@ -0,0 +1,5 @@
+export * from './scoring.service';
+export * from './scoring.controller';
+export * from './scoring.module';
+export * from './time-logs/time-log.model';
+export * from './performance-score.model';
